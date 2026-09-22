10d1a5c4ab3c355fdd4bbd95f7aadc655aaa03c0
manelcolominas <183344343+manelcolominas@users.noreply.github.com>
2026-09-22 15:25:39 +0200

    getting bits

--- main.py
+++ main.py
@@ -13,13 +13,17 @@ def run_app():
 
 
 def run_pipeline(binary_file_path: Path):
-    read_asterix_messages(binary_file_path)
+    bits = read_asterix_messages_bits(binary_file_path)
+    identify_type_of_asterix_messages(bits)
 
 
-def read_asterix_messages(binary_file_path: Path):
+def read_asterix_messages_bits(binary_file_path: Path):
     bits = Bits(filename=binary_file_path).b
     return bits
 
+
+def identify_type_of_asterix_messages(bits: str):
+    pass
 
 if __name__ == "__main__":
     run_app()
---
+++ models.py
@@ -0,0 +1,12 @@
+class AsterixMessage:
+    category:
+    length: int
+    data_record: DataRecord
+
+
+class DataRecord:
+    fspec:
+    data_field: DataField
+
+
+class DataField
