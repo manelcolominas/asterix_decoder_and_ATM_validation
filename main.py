d572397f6959a0963ffe33122a9b3ed0e87d29b2
manelcolominas <183344343+manelcolominas@users.noreply.github.com>
2026-09-22 14:41:48 +0200

    getting bits

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
+from pathlib import Path
+from bitstring import Bits
+
+
 def run_app():
-    run_pipeline()
+    binary_file_path = Path(
+        r"C:\Users\manel\Documents\Educació\Universitat\6è any\1r Quadrimestre"
+        r"\Projectes per a la Gestió del Trànsit Aeri\Projectes\Projecte 2 i 3"
+        r"\asterix_radar.ast"
+    )
 
-def run_pipeline():
-    print("Hello World")
+    run_pipeline(binary_file_path)
 
-run_app()
+
+def run_pipeline(binary_file_path: Path):
+    read_asterix_messages(binary_file_path)
+
+
+def read_asterix_messages(binary_file_path: Path):
+    bits = Bits(filename=binary_file_path).b
+    return bits
+
+
+if __name__ == "__main__":
+    run_app()
