2ea82fbe33168c78366675562d72b04cbfaa09e8
manelcolominas <183344343+manelcolominas@users.noreply.github.com>
2026-09-25 10:09:40 +0200

    fixing

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pathlib import Path
 
 from models import FRN_MAPS, ITEM_SPECS, AsterixMessage, CategoryMessage, DataRecord, DataField, DataItem, DataItemType, DataFieldType
 
+from cat021_decoder import decode_data_item_cat021
+from cat048_decoder import decode_data_item_cat048
 
 INTERESTING_DATA_ITEMS = {
     DataItemType.I021_010,
@@ -99,14 +101,8 @@ def decode_records_message(category: CategoryMessage, data: bytes) -> list[DataR
             if bit == 0:
                 continue
             item_type = map_frn_to_item_type(category, frn)
-            if item_type == DataItemType.I048_130:
-                print(DataItemType.I048_130)
-            # if item_type is None:
-            #     raise ValueError(
-            #         f"FRN no mapat: categoria={category.name}, "
-            #         f"frn={frn}, fspec={fspec}, offset={offset}"
-            #     )
-            item, offset = decode_data_item(item_type, data, offset)
+            print(item_type)
+            item, offset = decode_data_item(category,item_type, data, offset)
             field = DataField(item=item,field_type=get_field_type(item_type))
             
             if item_type in INTERESTING_DATA_ITEMS:
@@ -144,26 +140,36 @@ def get_field_type(item_type: DataItemType) -> DataFieldType:
     return ITEM_SPECS[item_type].field_type
 
 
-def decode_data_item(item_type: DataItemType, data: bytes, offset: int) -> tuple[DataItem, int]:
+def decode_data_item(category: CategoryMessage, item_type: DataItemType, data: bytes, offset: int) -> tuple[DataItem, int]:
     field_type = get_field_type(item_type)
 
     if field_type == DataFieldType.FIXED:
-        return decode_fixed_item(item_type, data, offset)
-    if field_type == DataFieldType.EXTENDED:
-        return decode_extended_item(item_type, data, offset)
-    if field_type == DataFieldType.REPETITIVE:
-        return decode_repetitive_item(item_type, data, offset)
-    if field_type == DataFieldType.COMPOUND:
-        return decode_compound_item(item_type, data, offset)
+        raw_content, new_offset = decode_fixed_item(item_type, data, offset)
+    elif field_type == DataFieldType.EXTENDED:
+        raw_content, new_offset = decode_extended_item(item_type, data, offset)
+    elif field_type == DataFieldType.REPETITIVE:
+        raw_content, new_offset = decode_repetitive_item(item_type, data, offset)
+    elif field_type == DataFieldType.COMPOUND:
+        raw_content, new_offset = decode_compound_item(item_type, data, offset)
+    elif field_type == DataFieldType.LENGTH_INDICATED:
+        raw_content, new_offset = decode_length_indicated_item(item_type, data, offset)
+    else:
+        raise ValueError(f"No decode rule defined for {item_type}")
 
-    raise ValueError(f"No decode rule defined for {item_type}")
+    if category == CategoryMessage.CAT021:
+        subfield = decode_data_item_cat021(item_type, raw_content)
+    elif category == CategoryMessage.CAT048:
+        subfield = decode_data_item_cat048(item_type, raw_content)
+    else:
+        raise ValueError(f"Unsupported category: {category}")
+
+    return DataItem(item_type=item_type,content=subfield), new_offset
 
 
-def decode_fixed_item(item_type: DataItemType, data: bytes, offset: int) -> tuple[DataItem, int]:
+def decode_fixed_item(item_type: DataItemType, data: bytes, offset: int) -> tuple[bytes, int]:
     length = ITEM_SPECS[item_type].length
     content = data[offset:offset + length]
-    data_item = DataItem(item_type=item_type, content=content)
-    return data_item, offset + length
+    return content, offset + length
 
 
 def decode_extended_item(item_type: DataItemType, data: bytes, offset: int) -> tuple[DataItem, int]:
@@ -203,6 +209,20 @@ def decode_compound_item(item_type: DataItemType, data: bytes, offset: int) -> t
     content = data[start_offset:end_offset]
 
     return DataItem(item_type=item_type, content=content), end_offset
+
+def decode_length_indicated_item(item_type: DataItemType, data: bytes, offset: int) -> tuple[bytes, int]:
+    length = data[offset]
+
+    if length < 1:
+        raise ValueError(f"Invalid length for {item_type}: {length}")
+
+    end_offset = offset + length
+
+    if end_offset > len(data):
+        raise ValueError(f"Truncated data item: {item_type}")
+
+    content = data[offset:end_offset]
+    return content, end_offset
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -21,7 +21,7 @@ class DataRecord:
 
 
 class DataItem:
-    def __init__(self,item_type: DataItemType, content: Any):
+    def __init__(self,item_type: DataItemType, content: list[DataItemSubfield]):
         self.item_type = item_type
         self.content = content
 
@@ -37,6 +37,7 @@ class DataFieldType(Enum):
     EXTENDED = "extended"
     REPETITIVE = "repetitive"
     COMPOUND = "compound"
+    LENGTH_INDICATED= "length_indicated"
 
 
 class DataItemType(str, Enum):
@@ -50,7 +51,7 @@ class DataItemType(str, Enum):
     I021_170 = "I021/170"
     I021_REF = "I021/REF"
 
-    # NOT INTERESTING DataItem's
+    # NOT INTERESTING DataItem's for CAT021
 
     I021_161 = "I021/161"
     I021_015 = "I021/015"
@@ -121,6 +122,10 @@ class DataItemType(str, Enum):
     SP_DATA_ITEM = "SP_DATA_ITEM"
     RE_DATA_ITEM = "RE_DATA_ITEM"
 
+class DataItemSubfield:
+    def __init__(self, pos: int, content: list[Any]):
+        self.pos = pos
+        self.content = content
 
 class DataItemTypeSpec:
     def __init__(self, field_type: DataFieldType, length: int | None = None, repetition_size: int | None = None, subfield_lengths: tuple[int, ...] = ()):
@@ -140,10 +145,7 @@ ITEM_SPECS = {
     DataItemType.I021_131: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=8),
     DataItemType.I021_145: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
     DataItemType.I021_170: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=6),
-    DataItemType.I021_REF: DataItemTypeSpec(
-        field_type=DataFieldType.COMPOUND,
-        subfield_lengths=(1, 1, 1, 1, 1, 1, 1),
-    ),
+    DataItemType.I021_REF: DataItemTypeSpec(field_type=DataFieldType.COMPOUND,subfield_lengths=(1, 1, 1, 1, 1, 1, 1)),
 
     # CAT021 no interessants
     DataItemType.I021_161: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
@@ -151,15 +153,44 @@ ITEM_SPECS = {
     DataItemType.I021_071: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=3),
     DataItemType.I021_130: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=6),
     DataItemType.I021_072: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=3),
+    DataItemType.I021_150: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+
+
+    DataItemType.I021_151: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I021_074: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
+    DataItemType.I021_075: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=3),
+    DataItemType.I021_076: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
+    DataItemType.I021_140: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+
     DataItemType.I021_090: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
+    DataItemType.I021_210: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
+    DataItemType.I021_230: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I021_152: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+
+    DataItemType.I021_200: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
+    DataItemType.I021_155: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I021_157: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I021_160: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
+
+    DataItemType.I021_165: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I021_077: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=3),
+    DataItemType.I021_020: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
     DataItemType.I021_220: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
+
+    DataItemType.I021_146: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I021_148: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
     DataItemType.I021_110: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
+    DataItemType.I021_016: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
+
+    DataItemType.I021_008: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
     DataItemType.I021_271: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
+    DataItemType.I021_132: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
+    DataItemType.I021_250: DataItemTypeSpec(field_type=DataFieldType.REPETITIVE, repetition_size=8),
+
+    DataItemType.I021_260: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=7),
+    DataItemType.I021_400: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
     DataItemType.I021_295: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
-    DataItemType.I021_250: DataItemTypeSpec(
-        field_type=DataFieldType.REPETITIVE,
-        repetition_size=8,
-    ),
+    DataItemType.I021_SPF: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
 
     # CAT048 interessants
     DataItemType.I048_010: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
@@ -168,20 +199,37 @@ ITEM_SPECS = {
     DataItemType.I048_040: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
     DataItemType.I048_070: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
     DataItemType.I048_090: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
-    DataItemType.I048_130: DataItemTypeSpec(
-        field_type=DataFieldType.COMPOUND,
-        subfield_lengths=(1, 1, 1, 1, 1, 1, 1),
-    ),
+    DataItemType.I048_130: DataItemTypeSpec(field_type=DataFieldType.LENGTH_INDICATED),
+
+    # DataItemType.I048_130: DataItemTypeSpec(field_type=DataFieldType.COMPOUND,subfield_lengths=(1, 1, 1, 1, 1, 1, 1)),
     DataItemType.I048_220: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=3),
     DataItemType.I048_240: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=6),
-    DataItemType.I048_250: DataItemTypeSpec(
-        field_type=DataFieldType.REPETITIVE,
-        repetition_size=8,
-    ),
+    DataItemType.I048_250: DataItemTypeSpec(field_type=DataFieldType.REPETITIVE,repetition_size=8),
     DataItemType.I048_161: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+
     DataItemType.I048_170: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
     DataItemType.I048_200: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
     DataItemType.I048_230: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+
+    # CAT048 no interessants
+    DataItemType.I048_042: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
+    
+    DataItemType.I048_210: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
+    DataItemType.I048_030: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
+    DataItemType.I048_080: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+
+    DataItemType.I048_100: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=4),
+    DataItemType.I048_110: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I048_120: DataItemTypeSpec(field_type=DataFieldType.EXTENDED),
+    DataItemType.I048_260: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=7),
+
+    DataItemType.I048_055: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
+    DataItemType.I048_050: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+    DataItemType.I048_065: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=1),
+    DataItemType.I048_060: DataItemTypeSpec(field_type=DataFieldType.FIXED, length=2),
+
+    DataItemType.SP_DATA_ITEM: DataItemTypeSpec(field_type=DataFieldType.LENGTH_INDICATED),
+    DataItemType.RE_DATA_ITEM: DataItemTypeSpec(field_type=DataFieldType.LENGTH_INDICATED),
 }
 
 FRN_MAPS = {
@@ -193,6 +241,7 @@ FRN_MAPS = {
         5: DataItemType.I021_071,
         6: DataItemType.I021_130,
         7: DataItemType.I021_131,
+
         8: DataItemType.I021_072,
         9: DataItemType.I021_150,
         10: DataItemType.I021_151,
@@ -200,6 +249,7 @@ FRN_MAPS = {
         12: DataItemType.I021_073,
         13: DataItemType.I021_074,
         14: DataItemType.I021_075,
+
         15: DataItemType.I021_076,
         16: DataItemType.I021_140,
         17: DataItemType.I021_090,
@@ -207,6 +257,7 @@ FRN_MAPS = {
         19: DataItemType.I021_070,
         20: DataItemType.I021_230,
         21: DataItemType.I021_145,
+
         22: DataItemType.I021_152,
         23: DataItemType.I021_200,
         24: DataItemType.I021_155,
@@ -214,6 +265,7 @@ FRN_MAPS = {
         26: DataItemType.I021_160,
         27: DataItemType.I021_165,
         28: DataItemType.I021_077,
+
         29: DataItemType.I021_170,
         30: DataItemType.I021_020,
         31: DataItemType.I021_220,
@@ -221,14 +273,17 @@ FRN_MAPS = {
         33: DataItemType.I021_148,
         34: DataItemType.I021_110,
         35: DataItemType.I021_016,
+
         36: DataItemType.I021_008,
         37: DataItemType.I021_271,
         38: DataItemType.I021_132,
         39: DataItemType.I021_250,
         40: DataItemType.I021_260,
         41: DataItemType.I021_400,
+
         42: DataItemType.I021_295,
         48: DataItemType.I021_REF,
+        49: DataItemType.I021_SPF,
     },
 
     CategoryMessage.CAT048: {
@@ -239,6 +294,7 @@ FRN_MAPS = {
         5: DataItemType.I048_070,
         6: DataItemType.I048_090,
         7: DataItemType.I048_130,
+
         8: DataItemType.I048_220,
         9: DataItemType.I048_240,
         10: DataItemType.I048_250,
@@ -246,6 +302,7 @@ FRN_MAPS = {
         12: DataItemType.I048_042,
         13: DataItemType.I048_200,
         14: DataItemType.I048_170,
+
         15: DataItemType.I048_210,
         16: DataItemType.I048_030,
         17: DataItemType.I048_080,
@@ -253,6 +310,7 @@ FRN_MAPS = {
         19: DataItemType.I048_110,
         20: DataItemType.I048_120,
         21: DataItemType.I048_230,
+
         22: DataItemType.I048_260,
         23: DataItemType.I048_055,
         24: DataItemType.I048_050,
